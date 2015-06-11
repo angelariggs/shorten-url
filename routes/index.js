bbb3a21959e217b96ecde91cbd9fcbad4d6efff9
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-11 13:02:25 -0700

    trying to fix the whitespace

--- routes/index.js
+++ routes/index.js
@@ -12,59 +12,81 @@ router.get('/', function(req, res, next) {
  res.render('index');
 });
 MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
-
 router.post('/', function(request, response) {
 var shortKey = uuid.v4();
 var target= request.body.URL
 console.log("howdy")
 
-var collection =  db.collection('shorten_url')
-collection.find(
-  {'target': {$eq: target}},
-  { target: 1, shortened:1}
-  ).toArray(function(err,reply){
-    if (reply.length>0){
-    console.log(target)
-    console.log(reply)
-    response.redirect(reply[0].shortened);
-    } else {
-      collection.insert(
-      {
-       "shortened": shortKey,
-       "target": target,
-       "clicks": 0,
-       "last_click": "2015-01-13T16:42:00"
-      }), function(err, docs) {
-          collection.find().toArray(function(err, results) {
-            console.dir(results);
-            response.redirect(shortKey);
-          });//closes collection.find
-        });//closes function
-    }//closes else
-  }//closes .toArray
+ var collection =  db.collection('shorten_url')
+ collection.find(
+       {'target': {$eq: target}},
+       { target: 1, shortened:1}
+       ).toArray(function(err,reply){
+         if (reply.length>0){
+           console.log(target)
+           console.log(reply)
+           response.redirect(reply[0].shortened);
+         }else{
+             collection.insert({
+                         "shortened": shortKey,
+                         "target": target,
+                         "clicks": 0,
+                         "last_click": "2015-01-13T16:42:00"}, function(err, docs) {
+                             collection.find().toArray(function(err, results) {
+                             console.dir(results);
+                            
+                           response.redirect(shortKey);
+
+               });
+             });
+         }
+       })
 
 });  
 
 
 router.get('/:shortUrl', function(request, response){
-  console.log('here')
-  var collection = db.collection('shorten_url')
-  var    shortUrl = request.params.shortUrl;
-  console.log(shortUrl)
-  
-  collection.update(
-    {shortened: {$eq: shortUrl}},
-    {$inc: {clicks: 1}}
-  )//closes .update
-  collection.find(
-    {shortened: {$eq: shortUrl}},
-    { target: 1, shortened: 1, clicks:1}
-  )//closes .find
-  .toArray(function(err, result){  
+// var countClicks = function() { 
+//  db.shorten_url.find(
+//  {"shortened": request.param.shortUrl},
+//  function(err, results) {
+//    console.log(results);
+// }//closes function
+// )};//closes .find
+
+  //closes .update
+// };
+console.log('here')    // MongoClient.connect('mongodb://127.0.0.1:27017/shorten', function(err, db) {
+ var collection = db.collection('shorten_url')
+
+ // var shortKey = uuid.v4();
+ var    shortUrl = request.params.shortUrl;
+ console.log(shortUrl)
+ // collection.find().toArray(function(err, url) {
+   collection.update(
+           {shortened: {$eq: shortUrl}},
+           {$inc: {clicks: 1}
+           })
+   collection.find(
+       {shortened: {$eq: shortUrl}},
+       { target: 1, shortened: 1, clicks:1}
+       ).toArray(function(err, result){
+           
+         console.log(result)
+     // console.log(err)
+     // console.log(url)
    response.render('post', {"url": result[0]});
-  })//closes .toArray
-});//closes router.get
-});//closes MongoClient
+ })
+     // })
+});
+});
+
+// db.collection.find(
+// {shortened:shortKey },
+// {
+//   target: 1
+// }
+//   ).limit(1)
 
 
 module.exports = router;
